9d2bfe73f7ee61e4d42c5fe2a992bed829a44c70
Vaibhavi Agale <vaibhaviagale7799@gmail.com>
2026-03-08 21:14:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "/src": path.resolve(__dirname, "./src"),
     },
   },
 }));
